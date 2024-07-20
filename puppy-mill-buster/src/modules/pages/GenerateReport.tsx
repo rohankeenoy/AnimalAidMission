3b83e4e02adea6c09d111659cb6b18d8cf69d527
jas2hf <jas2hf@mail.missouri.edu>
2024-07-20 14:54:32 -0500

    Final commit

--- puppy-mill-buster/src/modules/pages/GenerateReport.tsx
+++ puppy-mill-buster/src/modules/pages/GenerateReport.tsx
@@ -1,23 +1,23 @@
 import { Navbar } from "./Navbar"
-import { useEffect } from "react"
-import axios from "axios"
+// import { useEffect } from "react"
+// import axios from "axios"
 
 
 export const GenerateReport =() => {
-    const getData = async () => {
-        //I have no idea if this will work. Do we need a state to save the data?
-        axios({
-            url: "https://localhost:5000/model/predict?address=611 Gravois Rd, Fenton, MO, 63026",
-            method: "GET"
-        })
-        .then((res) => {console.log(res)})
+    // const getData = async () => {
+    //     //I have no idea if this will work. Do we need a state to save the data?
+    //     axios({
+    //         url: "https://localhost:5000/model/predict?address=611 Gravois Rd, Fenton, MO, 63026",
+    //         method: "GET"
+    //     })
+    //     .then((res) => {console.log(res)})
         
-        .catch((err) => {console.log(err)})
-    }
+    //     .catch((err) => {console.log(err)})
+    // }
     
-    useEffect(() => {
-        getData()
-    }, [])
+    // useEffect(() => {
+    //     getData()
+    // }, [])
 
     return(
       <>
