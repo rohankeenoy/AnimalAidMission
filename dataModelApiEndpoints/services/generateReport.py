7705368658fa67294d901f8d7f5678b0222c9792
Rohan Keenoy <rohankeenoy@gmail.com>
2024-07-20 14:07:11 -0500

    figuring out graph

--- dataModelApiEndpoints/services/generateReport.py
+++ dataModelApiEndpoints/services/generateReport.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import BallTree
 import os
 import requests
 import plotly.express as px
+import matplotlib.pyplot as plt
 
 report_bp = Blueprint('report', __name__)
 
@@ -17,29 +18,28 @@ def getTheVis():
     basePP = os.path.join(os.path.dirname(__file__),"predicted_point.csv")
     nearestNeighbors = pd.read_csv(baseNN)
     pp = pd.read_csv(basePP)
-    fig = px.scatter_mapbox()
-        
-        # Add nearest neighbors data
-    fig.add_scattermapbox(
-            lat=nearestNeighbors['Latitude'],
-            lon=nearestNeighbors['Longitude'],
-            mode='markers',
-            marker=dict(size=10, color='blue'),
-            name='Nearest Neighbors'
-        )
-        
+    NN = pd.DataFrame(nearestNeighbors)
+    pp = pd.DataFrame(pp)
     
-        # Update layout
-    fig.update_layout(
-            mapbox_style="open-street-map",
-            margin={"r":0, "t":0, "l":0, "b":0},
-            legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.01)
-        )
+    print("head",NN.head())
+    print("pp",pp.head())
+    plt.figure(figsize=(10, 8))
         
-        # Convert figure to JSON-serializable format
-    figure_json = fig.to_json()
+    plt.scatter(nearestNeighbors['Longitude'], nearestNeighbors['Latitude'], color='blue', label='Nearest Neighbors')
+        
+    plt.scatter(pp['longitude'], pp['latitude'], color='red', label='Viewed Puppy')
+        
+        # Set labels and title
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
+    plt.title('Geographical Points')
+        
+        # Add legend
+    plt.legend()
+    combined_df = pd.concat([NN, pp], ignore_index=True)
+    fig = px.scatter_mapbox(combined_df, lat="Latitude", lon="Longitude",color_discrete_sequence=["blue", "red"], zoom=3, height=600)
     
-    
+    fig.write_image("plot_combined.png")
     if not pp.empty and not nearestNeighbors.empty:
         return jsonify({"message": "CSV files loaded successfully"}), 200
     else:
