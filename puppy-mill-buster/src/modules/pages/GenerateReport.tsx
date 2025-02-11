85b4b0c17542cfe635dbb9d2f5c5f76fe3ff94db
Rohan Keenoy <rohankeenoy@gmail.com>
2025-02-10 19:55:12 -0600

    oops change in report

--- puppy-mill-buster/src/modules/pages/GenerateReport.tsx
+++ puppy-mill-buster/src/modules/pages/GenerateReport.tsx
@@ -1,34 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import {Navbar} from './Navbar'; 
+import React from 'react';
+import { Navbar } from './Navbar';
 
-export const GenerateReport =() => {
-    const getData = async () => {
-        //I have no idea if this will work. Do we need a state to save the data?
-        axios({
-            url: "https://localhost:5000/model/predict?address=611 Gravois Rd, Fenton, MO, 63026",
-            method: "GET"
-        })
-        .then((res) => {console.log(res)})
-        
-        .catch((err) => {console.log(err)})
-    }
-    
-    useEffect(() => {
-        getData()
-    }, [])
-
+export const GenerateReport = () => {
   return (
     <>
       <Navbar />
       <div>
-        <p>Write your content here</p>
-        {isDataLoaded && (
-          <div>
-            <p>Visualization goes here:</p>
-            
-          </div>
-        )}
+        <p>Report Page! Report stuff here</p>
       </div>
     </>
   );
